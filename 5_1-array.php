0dec3e868c9e69b39f0963bc9f75f0156ab1ea20
meidina <meidinasetia@gmail.com>
2016-10-11 21:43:41 +0700

    Add files via upload

---
+++ 5_1-array.php
@@ -0,0 +1,33 @@
+<html>
+<head>
+<title>studi kasus 5</title>
+</head>
+<body>
+	<?php
+	$toko = array
+	(
+		array(1001,"Sabun Lifebuoy","Rp.1.500,-"),
+		array(1002,"Permen Blaster","Rp.5.600,-"),
+		array(1003,"Pepsodent","Rp.4.560,-"),
+		array(1004,"Madu Arbian","Rp.30.000,-"),
+		array(1005,"Kecap ABC","Rp7.250,-"),
+		array(1006,"Sauce Tomat ABC","Rp.6.700,-"),
+		array(1007,"Gulaku","Rp.8.900,-"),
+		array(1008,"Rinso","Rp.7.100,-"),
+		array(1009,"Super Pel","Rp.6.450,-"),
+		array(1010,"Permen Tanggo","Rp.5.600,-")
+		);
+	
+	echo "Kode Barang = ".$toko[0][0]." || Nama Barang =  ".$toko[0][1]." || Harga Barang = ".$toko[0][2]."<br>";
+	echo "Kode Barang = ".$toko[1][0]." || Nama Barang =  ".$toko[1][1]." || Harga Barang = ".$toko[1][2]."<br>";
+	echo "Kode Barang = ".$toko[2][0]." || Nama Barang =  ".$toko[2][1]." || Harga Barang = ".$toko[2][2]."<br>";
+	echo "Kode Barang = ".$toko[3][0]." || Nama Barang =  ".$toko[3][1]." || Harga Barang = ".$toko[3][2]."<br>";
+	echo "Kode Barang = ".$toko[4][0]." || Nama Barang =  ".$toko[4][1]." || Harga Barang = ".$toko[4][2]."<br>";
+	echo "Kode Barang = ".$toko[5][0]." || Nama Barang =  ".$toko[5][1]." || Harga Barang = ".$toko[5][2]."<br>";
+	echo "Kode Barang = ".$toko[6][0]." || Nama Barang =  ".$toko[6][1]." || Harga Barang = ".$toko[6][2]."<br>";
+	echo "Kode Barang = ".$toko[7][0]." || Nama Barang =  ".$toko[7][1]." || Harga Barang = ".$toko[7][2]."<br>";
+	echo "Kode Barang = ".$toko[8][0]." || Nama Barang =  ".$toko[8][1]." || Harga Barang = ".$toko[8][2]."<br>";
+	echo "Kode Barang = ".$toko[9][0]." || Nama Barang =  ".$toko[9][1]." || Harga Barang = ".$toko[9][2]."<br>";
+	?>
+	</body>
+	</html> 
